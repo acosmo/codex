3ce85995c3b809c702d6287fe7c4588b42eb9b2d
Abc Abc <health@gmail.com>
2026-05-03 23:13:33 +0100

    12

--- stats/get.sh
+++ stats/get.sh
@@ -4,6 +4,7 @@ IP="$1"
 
 awk -F'|' -v ip="$IP" '
 $2 ~ ip {
+    date=$1
     book=$3
     verse=$4
     text=$5
@@ -12,14 +13,15 @@ $2 ~ ip {
     sub(/.*VERSE:/, "", verse)
     sub(/.*TEXT:/, "", text)
 
+    gsub(/^ +| +$/, "", date)
     gsub(/^ +| +$/, "", book)
     gsub(/^ +| +$/, "", verse)
     gsub(/^ +| +$/, "", text)
 
     key=book ":" verse
-    data[key]=book " " verse " | " text
+    data[key]=date " | " book " " verse " | " text
 }
 END {
     for (k in data)
         print data[k]
-}' "$LOG_FILE" | sort
+}' "$LOG_FILE" | sort -r
